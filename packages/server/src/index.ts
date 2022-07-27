26bf2908ed9275ed5540decdc2404cb6e1f2832e
Erin Clark <erincodepirate@gmail.com>
2022-07-26 18:15:25 -0700

    Add graphql schema and cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,43 +1,78 @@
 import express, { Application } from 'express';
-import { ApolloServer, Config, gql } 
+import { ApolloServer, } 
   from 'apollo-server-express';
-import { IResolvers } from '@graphql-tools/utils';
+import schema from './graphql/schema';
+import casual from 'casual';
+import cors from 'cors';
 
-// express stuff
-const PORT = 8080;
-const app: Application = express();
+let postsIds: string[] = [];
+let usersIds: string[] = []; 
 
-// graphql stuff
-const typeDefs = gql`type Query {message: String!}`;
-const resolvers: IResolvers = {
-  Query: {
-    message: () => 'It works!'
-  }
+const mocks = {
+  User: () => ({
+    id: () => {let uuid = casual.uuid; usersIds.push(uuid); 
+               return uuid},
+    fullName: casual.full_name,
+    bio: casual.text,
+    email: casual.email,
+    username: casual.username,
+    password: casual.password,
+    image: 'https://picsum.photos/seed/picsum/200/300',
+    coverImage: 
+     'https://picsum.photos/seed/picsum/200/300',
+    postsCount: () => casual.integer(0)
+  }),
+  Post: () => ({
+    id: () => {let uuid = casual.uuid; postsIds.push(uuid);
+               return uuid},
+    text: casual.text,
+    author: casual.random_element(usersIds),
+    image: 'https://picsum.photos/seed/picsum/200/300',
+    commentsCount: () => casual.integer(0),
+    likesCount: () => casual.integer(0),
+    latestLike: casual.first_name,
+    createdAt: () => casual.date()
+  }),
+  Comment: () => ({
+    id: casual.uuid,
+    comment: casual.text,
+    author: casual.random_element(usersIds),
+    post: casual.random_element(postsIds),
+    createdAt: () => casual.date()
+  }),
+  Like: () => ({
+    id: casual.uuid,
+    post: casual.random_element(postsIds),
+    user: casual.random_element(usersIds),
+  }),
+  Query: () =>({
+    getPostsByUserId: () => 
+      [...new Array(casual.integer(10, 100))],
+    getFeed: () => [...new Array(casual.integer(10, 100))],
+    getNotificationsByUserId: () => 
+      [...new Array(casual.integer(10, 100))],
+    getCommentsByPostId: () => 
+      [...new Array(casual.integer(10, 100))],
+    getLikesByPostId: () =>
+      [...new Array(casual.integer(10, 100))],
+    searchUsers: () =>
+      [...new Array(casual.integer(10, 100))]
+  })
 };
 
-const config: Config = {
-  typeDefs: typeDefs,
-  resolvers: resolvers
-};
-
-/*app.get('/', (req, res) =>
-  res.send('Express is successfully running!')); 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});*/
-
-async function startApolloServer(config: Config) {
+async function startApolloServer() {
   const PORT = 8080;
   const app: Application = express();
   const server: ApolloServer =
-    new ApolloServer(config);
+    new ApolloServer({schema, mocks, mockEntireSchema: false});
   await server.start();
   server.applyMiddleware({
     app,
     path: '/graphql'
   });
+  app.use(cors());
   app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
   });
 }
-startApolloServer(config);
+startApolloServer();
