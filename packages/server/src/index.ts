2e906cab33f55014d585a15faa0944af9868b586
Erin Clark <erincodepirate@gmail.com>
2022-07-26 17:39:14 -0700

    Add basic graphql

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,43 @@
 import express, { Application } from 'express';
+import { ApolloServer, Config, gql } 
+  from 'apollo-server-express';
+import { IResolvers } from '@graphql-tools/utils';
+
+// express stuff
 const PORT = 8080;
 const app: Application = express();
-app.get('/', (req, res) =>
+
+// graphql stuff
+const typeDefs = gql`type Query {message: String!}`;
+const resolvers: IResolvers = {
+  Query: {
+    message: () => 'It works!'
+  }
+};
+
+const config: Config = {
+  typeDefs: typeDefs,
+  resolvers: resolvers
+};
+
+/*app.get('/', (req, res) =>
   res.send('Express is successfully running!')); 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
-});
+});*/
+
+async function startApolloServer(config: Config) {
+  const PORT = 8080;
+  const app: Application = express();
+  const server: ApolloServer =
+    new ApolloServer(config);
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql'
+  });
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+startApolloServer(config);
